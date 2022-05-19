87153d2a51fa11a25ad06c5d583a2f885b25b4fa
akoji@1982 <akoji1982@gmail.com>
2022-05-19 05:30:05 -0700

    akoji

---
+++ help_func/calloc.c
@@ -0,0 +1,29 @@
+#include "shell.h"
+#include <stdlib.h>
+
+/**
+ * _calloc - concatenate two strings
+ * @nmemb: number of elements
+ * @size: type of elements
+ * Return: nothing
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *p = NULL;
+	unsigned int i;
+
+	if (nmemb == 0 || size == 0)
+	{
+		return (NULL);
+	}
+	p = malloc(nmemb * size);
+	if (p == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < (nmemb * size); i++)
+	{
+		*((char *)(p) + i) = 0;
+	}
+	return (p);
+}
